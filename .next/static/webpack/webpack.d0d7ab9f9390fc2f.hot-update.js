02f10b82084b53d8ce8654181b932b1eb96d56ce
hayeong <fibonacci2246@gmail.com>
2022-11-08 17:01:07 +0900

    fix: styled-components 정적 페이지에서 적용안되는 버그 _document부분에서 삽입해주어 해결

--- .next/static/webpack/webpack.d0d7ab9f9390fc2f.hot-update.js
+++ .next/static/webpack/webpack.d0d7ab9f9390fc2f.hot-update.js
@@ -7,19 +7,12 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-self["webpackHotUpdate_N_E"](
-  "webpack",
-  {},
-  /******/ function (__webpack_require__) {
-    // webpackRuntimeModules
-    /******/ /* webpack/runtime/getFullHash */
-    /******/ !(function () {
-      /******/ __webpack_require__.h = function () {
-        return "a58699e38aef017b";
-      };
-      /******/
-    })();
-    /******/
-    /******/
-  }
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "6f6ca3b87696dc7c"; }
+/******/ }();
+/******/ 
+/******/ }
 );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,34 @@ import Document, {
   NextScript,
   DocumentContext
 } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheets = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheets.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheets.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheets.seal();
+    }
   }
 
   render() {
